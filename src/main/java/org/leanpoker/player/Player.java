11562293f5c3bf7f61b16248e677bbbace710187
Thomas von Siebenthal <thomas.vonsiebenthal@adcubum.com>
2017-11-13 14:17:29 +0100

    stack / 2 setzen

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,32 +1,36 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 public class Player {
 
-    static final String VERSION = "SEM Java folding player Version 0.0.8";
+    static final String VERSION = "SEM Java folding player Version 0.0.9";
+    public static final String SEMPOKER = "sempoker";
+    public static final String ACTIVE = "active";
 
     public static int betRequest(JsonElement request) {
-        return 1000;
-
-        /*
         JsonObject jsonObject = request.getAsJsonObject();
         int current_buy_in = jsonObject.get("current_buy_in").getAsInt();
         int minimum_raise = jsonObject.get("minimum_raise").getAsInt();
         JsonArray players = jsonObject.get("players").getAsJsonArray();
         int bets = 0;
+        int myStack = 0;
         for (JsonElement player : players) {
             JsonObject playerAsJsonObject = player.getAsJsonObject();
             String status = playerAsJsonObject.get("status").getAsString();
             String name = playerAsJsonObject.get("name").getAsString();
             int bet = playerAsJsonObject.get("bet").getAsInt();
-            if (!name.equals("sempoker") && status.equals("active")) {
+            if (!name.equals(SEMPOKER) && status.equals(ACTIVE)) {
                 bets += bet;
             }
+            if (name.equals(SEMPOKER)) {
+                myStack = playerAsJsonObject.get("stack").getAsInt();
+            }
         }
-//        return current_buy_in - bets + minimum_raise + 1;
-        return current_buy_in + 10;
-        */
+
+        return myStack / 2;
     }
 
     public static void showdown(JsonElement game) {
