78cbc03848be1d3fa62debcb171786051d38bf83
Thomas von Siebenthal <thomas.vonsiebenthal@adcubum.com>
2017-11-13 11:55:37 +0100

    immer mitbieten

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,15 +1,30 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 public class Player {
 
-    static final String VERSION = "SEM Java folding player Version 0.0.3";
+    static final String VERSION = "SEM Java folding player Version 0.0.4";
 
     public static int betRequest(JsonElement request) {
         System.out.println("betRequest: " + request);
 
-        return 10;
+        JsonObject jsonObject = request.getAsJsonObject();
+        int current_buy_in = jsonObject.get("current_buy_in").getAsInt();
+        int minimum_raise = jsonObject.get("minimum_raise").getAsInt();
+        JsonArray players = jsonObject.get("players").getAsJsonArray();
+        int bets = 0;
+        for (JsonElement player : players) {
+            JsonObject playerAsJsonObject = player.getAsJsonObject();
+            String status = playerAsJsonObject.get("status").getAsString();
+            int bet = playerAsJsonObject.get("bet").getAsInt();
+            if (status.equals("active")) {
+                bets += bet;
+            }
+        }
+        return current_buy_in - bets + minimum_raise;
     }
 
     public static void showdown(JsonElement game) {
