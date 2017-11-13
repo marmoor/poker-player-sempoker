2ffc5e7e5e68ae25870fd5e0c74dd9cc1c2a886c
Thomas von Siebenthal <thomas.vonsiebenthal@adcubum.com>
2017-11-13 11:39:29 +0100

    System.out eingebaut

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -4,12 +4,15 @@ import com.google.gson.JsonElement;
 
 public class Player {
 
-    static final String VERSION = "SEM Java folding player Version 0.0.1";
+    static final String VERSION = "SEM Java folding player Version 0.0.2";
 
     public static int betRequest(JsonElement request) {
+        System.out.println("betRequest: " + request);
+
         return 1;
     }
 
     public static void showdown(JsonElement game) {
+        System.out.println("showdown: " + game);
     }
 }
